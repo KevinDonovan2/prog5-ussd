f023bb65a62b035b5c2bb7e0eabbb1952647d9f0
KevinDonovan2 <hei.donovan.2@gmail.com>
2025-05-15 08:05:17 +0300

    feat: conf eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,32 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
-
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    rules: {
+      // Règles strictes ajoutées
+      'no-unused-vars': 'error',
+      eqeqeq: ['error', 'always'],
+      curly: ['error', 'all'],
+      strict: 'off',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      indent: ['error', 2],
+      quotes: ['error', 'single'],
+      semi: ['error', 'always']
+    }
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: { sourceType: 'commonjs' }
+  },
+  {
+    files: ['**/*.{js,mjs,cjs}'],
+    languageOptions: { globals: globals.browser }
+  }
 ]);
